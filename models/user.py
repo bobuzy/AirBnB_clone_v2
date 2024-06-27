0d3d5df39c5da77612f13694f70ac4f6b30669d8
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-06-27 21:18:02 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""This module defines a class Place"""
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-class Place(BaseModel, Base):
-    """Place class definition"""
+class User(BaseModel, Base):
+    """User class definition"""
 
     __tablename__ = "users"
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, default=0)
-    number_bathrooms = Column(Integer, default=0)
-    max_guest = Column(Integer, default=0)
-    price_by_night = Column(Integer, default=0)
-    user = relationship("User", backref="places")
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="all, delete")
+    reviews = relationship("Review", backref="user", cascade="all, delete")
