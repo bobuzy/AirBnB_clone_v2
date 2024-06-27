733cca07de70767a61b126056cd028336004446b
MmaGod1 <gmmaduchukwu@gmail.com>
2024-06-26 18:16:55 -0700

    fix spelling

--- models/place.py
+++ models/place.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
     """ Defines a Place class"""
     __tablename__ = 'places'
-    city_id = Column(String(60), nullable=False, ForiegnKey('cities.id'))
-    user_id = Column(String(60), nullable=False, ForiegnKey('users.id'))
+    city_id = Column(String(60), nullable=False, ForeinKey('cities.id'))
+    user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
     name = Column(String(128), nullable=false)  
     description = Column(String(1024), nullable=false)
     number_rooms = Column(Integer, nullable=False, default=0)
