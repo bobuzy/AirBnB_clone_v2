9f4ec0ad5cebaaafd8932761277b11cdb2172425
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-06-27 21:13:57 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import relationship
 class Place(BaseModel, Base):
     """Place class definition"""
 
-    __tablename__ = "places"
+    __tablename__ = "users"
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
