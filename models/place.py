ef75049c5141d86e4bfbfa44495da46b656eea8f
MmaGod1 <gmmaduchukwu@gmail.com>
2024-06-26 18:22:22 -0700

    fix spelling

--- models/place.py
+++ models/place.py
@@ -2,7 +2,6 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
@@ -11,11 +10,11 @@ class Place(BaseModel, Base):
     city_id = Column(String(60), nullable=False, ForeinKey('cities.id'))
     user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
     name = Column(String(128), nullable=false)  
-    description = Column(String(1024), nullable=false)
+    description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
