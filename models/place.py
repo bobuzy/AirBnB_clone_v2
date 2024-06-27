37219eef78ad9c4f0b58ad016f6b713ccda8e2d0
MmaGod1 <gmmaduchukwu@gmail.com>
2024-06-26 18:36:38 -0700

    fix spelling

--- models/place.py
+++ models/place.py
@@ -7,9 +7,9 @@ from sqlalchemy import Column, String, Integer, Float, ForeignKey
 class Place(BaseModel, Base):
     """ Defines a Place class"""
     __tablename__ = 'places'
-    city_id = Column(String(60), nullable=False, ForeinKey('cities.id'))
-    user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
-    name = Column(String(128), nullable=false)  
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)  
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
