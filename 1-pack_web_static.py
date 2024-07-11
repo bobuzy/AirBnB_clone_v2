8db622d50615fd8d7a3397b19b243f33d3475fde
vagrant <vagrant@vagrant.vm>
2024-07-11 13:25:38 +0000

    Make tar compression verbose

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""Fabric script to genereate tgz archive from web_static folder"""
-
+"""
+Fabric script to genereate tgz archive from web_static folder
+"""
 from fabric.api import *
 from datetime import datetime
 
 
 def do_pack():
-    """Create a .tgz archive from the contents of the web_static folder."""
-
+    """Create a .tgz archive from the contents of the web_static folder"""
     time = datetime.now()
     formatted_time = time.strftime('%Y%m%d%H%M%S')
 
     local('mkdir -p versions')
 
     archive = "versions/web_static_{}.tgz".format(formatted_time) 
-    result = local('tar -czf {} web_static'.format(archive))
+    result = local('tar -cvzf {} web_static'.format(archive))
 
     if result.ok:
         return archive
