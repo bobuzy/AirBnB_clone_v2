2493dd1022c6dd848e9f8ca5449dba74c5bdb9ad
vagrant <vagrant@vagrant.vm>
2024-07-11 21:23:23 +0000

    Deploy server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,28 +2,35 @@
 """
 Fabric script to distribute archive to the web servers
 """
-
 from fabric.api import *
 from os.path import exists
+from datetime import datetime
+
 env.hosts = ['54.237.36.103', '100.24.74.195']
 env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
 
 def do_deploy(archive_path):
     """Decompress and distribute archive to web servers"""
-    if exists(archive_path) is False:
-        return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        sudo('mkdir -p {}{}/'.format(path, no_ext))
-        sudo('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        sudo('rm /tmp/{}'.format(file_n))
-        sudo('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        sudo('rm -rf {}{}/web_static'.format(path, no_ext))
+        if not exists(archive_path):
+            return False
+        put(archieve_path, '/tmp/')
+
+        timestamp = archive_path[-18:-4]
+        sudo('mkdir -p /data/web_static/releases/
+              webstatic_{}'.format(timestamp))
+        sudo('tar -xzf /tmp/web_static_{0}.tgz -C /data/web_static/
+              releases/web_static_{0}/'.format(timestamp))
+        sudo('mv /data/web_static/releases/web_static_{0}/web_static/* \
+/data/web_static/releases/web_static_{0}/'.format(timestamp))
+        sudo('rm -rf /data/web_static/releases/\
+web_static_{}/web_static'.format(timestamp))
         sudo('rm -rf /data/web_static/current')
-        sudo('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except Exception:
+        sudo('ln -s /data/web_static/releases/\
+web_static_{}/ /data/web_static/current'.format(timestamp))
+    except:
         return False
+
+    return True
