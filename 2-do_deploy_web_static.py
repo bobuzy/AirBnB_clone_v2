2f5905c4258f7c088f65163c4595127057aa8cad
vagrant <vagrant@vagrant.vm>
2024-07-11 19:42:08 +0000

    Decompress and deploy .tgz archive file passed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
 """
-Fabric script to decompress tgz archive passed
+Fabric script that decompresses and deploys .tgz files passed 
 """
+
+from fabric.api import put, run, env
 from os.path import exists
-from fabric.api import *
 env.hosts = ['100.24.74.195', '54.237.36.103']
 
 
 def do_deploy(archive_path):
-    """Decompress archive_path passed"""
-    if not exists(archive_path):
+    """Distribute archive to the web servers abd deploy"""
+    if exists(archive_path) is False:
         return False
-
     try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        folder_exe = archive_path.split("/")[-1]
-        folder_name = folder_exe.split(".")[0]
-        destination = '/data/web_static/releases/'
-        run('mkdir -p {}{}/'.format(destination, folder_name))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(folder_exe,
-                                               destination, folder_name))
-        run('rm /tmp/{}'.format(folder_exe))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(destination,
-                                                    folder_name))
-        run('rm -rf {}{}/web_static'.format(destination, folder_exe))
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(destination,
-                                                          folder_name))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
