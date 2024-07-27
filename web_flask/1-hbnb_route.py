864b1167d895c9405f0e1516857f25d32ddd04e9
bobuzy <bobuzytech@gmail.com>
2024-07-27 18:26:26 +0000

    Rename funtion to avoid collision

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,7 +12,7 @@ def hello_HBNB():
     return "Hello HBNB!"
 
 @app.route("/hbnb", strict_slashes=False)
-def hello_HBNB():
+def HBNB():
     """Return 'HBNB' on '/hbnb' route"""
     return "HBNB"
 
