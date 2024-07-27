e659a62eca5a493960eead403b731e63f41c1ba7
bobuzy <bobuzytech@gmail.com>
2024-07-27 18:50:35 +0000

    Return a string 'C' followed by the value of the text variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A simple python flask script that listens on 0.0.0.0, port 5000"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """Return 'Hello HBNB!' on root route"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """Return 'HBNB' on '/hbnb' route"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def hello_c(text):
+    """Return 'C' with whatever text passed in the sub-route"""
+    return "C " + text.replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
