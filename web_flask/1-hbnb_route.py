70b55182397f3f9d36e388c13b44a688c702e20a
bobuzy <bobuzytech@gmail.com>
2024-07-27 18:27:37 +0000

    Add extra space

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,6 +11,7 @@ def hello_HBNB():
     """Return 'Hello HBNB!' on root route"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def HBNB():
     """Return 'HBNB' on '/hbnb' route"""
