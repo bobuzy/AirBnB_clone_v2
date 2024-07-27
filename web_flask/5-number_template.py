54de61c8a0fffbe6e38f78a34a03a34cd22e1d5c
bobuzy <bobuzytech@gmail.com>
2024-07-27 20:10:56 +0000

    Remove space

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -40,7 +40,7 @@ def is_number(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def int_template(n):
     """Render template if n is an integer"""
-    return render_template ("5-number.html", n=n)
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == '__main__':
