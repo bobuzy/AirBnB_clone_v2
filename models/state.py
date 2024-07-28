2296b45c7f09f21b437569eeb37871459f584e7f
bobuzy <bobuzytech@gmail.com>
2024-07-28 18:50:00 +0000

    Undo changes

--- models/state.py
+++ models/state.py
@@ -10,12 +10,9 @@ from os import getenv
 class State(BaseModel, Base):
     """ State class """
     
-    if models.storage_type == "db":
-        __tablename__ = "states"
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="delete")
-    else:
-        name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
