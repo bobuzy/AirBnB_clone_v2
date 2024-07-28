ada3300e1f9decab4c40230dfca76937e695ce36
bobuzy <bobuzytech@gmail.com>
2024-07-28 18:50:59 +0000

    Remove space

--- models/state.py
+++ models/state.py
@@ -9,7 +9,7 @@ from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
-    
+
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="delete")
