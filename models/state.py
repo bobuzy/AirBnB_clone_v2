ba520c587caedb92510ee490ed89fd9546574004
bobuzy <bobuzytech@gmail.com>
2024-07-28 18:56:45 +0000

    Change 'cities' type for db and non-db storage

--- models/state.py
+++ models/state.py
@@ -12,9 +12,10 @@ class State(BaseModel, Base):
 
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="delete")
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="delete")
+    else:
         @property
         def cities(self):
             """Return list of cities linked to a state"""
