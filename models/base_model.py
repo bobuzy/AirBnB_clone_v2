b588892add0a1fd0bdf89f9014951b6291178e7a
bobuzy <bobuzytech@gmail.com>
2024-07-28 21:04:43 +0000

    Update Basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -43,8 +43,10 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
+        dic = self.__dict__.copy()
+        dic.pop("_sa_instance_state", None)
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return '[{}] ({}) {}'.format(cls, self.id, dic)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
@@ -58,8 +60,7 @@ class BaseModel:
         dictionary["__class__"] = str(type(self).__name__)
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if '_sa_instance_state' in dictionary.keys():
-            del dictionary['_sa_instance_state']
+        dictionary.pop("_sa_instance_state", None)
         return dictionary
 
     def delete(self):
