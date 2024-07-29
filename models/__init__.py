6ea9a10cbf5c3705e35cbb426a83dc18ba81b0dd
bobuzy <bobuzytech@gmail.com>
2024-07-29 16:38:07 +0000

    Update init

--- models/__init__.py
+++ models/__init__.py
@@ -3,12 +3,12 @@
 from os import getenv
 
 
-"""storage_type = getenv("HBNB_TYPE_STORAGE")
-if storage_type == 'db':"""
-from models.engine.db_storage import DBStorage
-storage = DBStorage()
-storage.reload()
-"""else:
+storage_type = getenv("HBNB_TYPE_STORAGE")
+if storage_type == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()"""
+    storage.reload()
