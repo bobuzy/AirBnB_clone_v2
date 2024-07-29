3403afadcdda7ff8ab14faa5e80839751ae986e1
bobuzy <bobuzytech@gmail.com>
2024-07-29 16:32:50 +0000

    Update Database

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -31,16 +31,11 @@ class DBStorage:
 
     def __init__(self):
         """Initializze DBStorage object"""
-        """HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
         HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
         HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
-        HBNB_ENV = getenv('HBNB_ENV')"""
-        HBNB_MYSQL_USER = "hbnb_dev"
-        HBNB_MYSQL_PWD = "hbnb_dev_pwd"
-        HBNB_MYSQL_HOST = "localhost"
-        HBNB_MYSQL_DB = "hbnb_dev_db"
-        HBNB_ENV = "dev"
+        HBNB_ENV = getenv('HBNB_ENV')
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
                                       format(HBNB_MYSQL_USER,
                                              HBNB_MYSQL_PWD,
