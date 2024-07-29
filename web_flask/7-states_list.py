924ffb310077e03b0a1d58ebca9d316864324421
bobuzy <bobuzytech@gmail.com>
2024-07-29 17:08:28 +0000

    Use string

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""Simple python flask script to list all states in the Database"""
-
+"""script that starts a Flask web application"""
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
+
 
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
-def list_states():
-    """Render all states in a template"""
-    state_list = storage.all("State")
-    sorted_states = sorted(state_list.values(), key=lambda state: state.name)
-    return render_template("7-states_list.html", states=sorted_states)
+def states_list():
+    """state list"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def stop_session(exception=None):
-    """Clean-up session"""
+def teardown_session(exception=None):
+    """teardown session"""
     storage.close()
 
 
