66563b2e50288e36dc816339a22d90b4d9b78f0f
bobuzy <bobuzytech@gmail.com>
2024-07-29 16:04:13 +0000

    Update database

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,69 +1,73 @@
 #!/usr/bin/python3
-"""Module definition for DBStorage"""
-
-import models
-from models.base_model import BaseModel, Base
+"""Defines the DBStorage engine."""
+from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from os import getenv
-import sqlalchemy
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-
-classes = {
-    "Amenity": Amenity,
-    "City": City,
-    "Place": Place,
-    "Review": Review,
-    "State": State,
-    "User": User
-}
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
 
 
 class DBStorage:
-    """DBStrorage class definition"""
+    """Represents a database storage engine.
+
+    Attributes:
+        __engine (sqlalchemy.Engine): The working SQLAlchemy engine.
+        __session (sqlalchemy.Session): The working SQLAlchemy session.
+    """
+
     __engine = None
     __session = None
 
     def __init__(self):
-        """Initializze DBStorage object"""
+        """Initialize a new DBStorage instance."""
         self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
                                       format(getenv("HBNB_MYSQL_USER"),
                                              getenv("HBNB_MYSQL_PWD"),
                                              getenv("HBNB_MYSQL_HOST"),
                                              getenv("HBNB_MYSQL_DB")),
                                       pool_pre_ping=True)
-        if getenv('HBNB_ENV') == 'test':
+        if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Return a dictionary of all/selected class objects"""
+        """Query on the curret database session all objects of the given class.
+
+        If cls is None, queries all types of objects.
+
+        Return:
+            Dict of queried classes in the format <class name>.<obj id> = obj.
+        """
         if cls is None:
-            objs = []
-            for cls_name in classes.values():
-                objs.extend(self.__session.query(cls_name).all())
+            objs = self.__session.query(State).all()
+            objs.extend(self.__session.query(City).all())
+            objs.extend(self.__session.query(User).all())
+            objs.extend(self.__session.query(Place).all())
+            objs.extend(self.__session.query(Review).all())
+            objs.extend(self.__session.query(Amenity).all())
         else:
             if type(cls) == str:
-                cls = classes.get(cls, None)
-            if cls is None:
-                return {}
-            objs = self.__session.query(cls).all()
+                cls = eval(cls)
+            objs = self.__session.query(cls)
         return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
 
     def new(self, obj):
-        """Add new object to the current database session"""
+        """Add obj to the current database session."""
         self.__session.add(obj)
 
     def save(self):
-        """Commit all changes to the current database session"""
+        """Commit all changes to the current database session."""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Delete object from the current database session"""
+        """Delete obj from the current database session."""
         if obj is not None:
             self.__session.delete(obj)
 
@@ -75,14 +79,6 @@ class DBStorage:
         Session = scoped_session(session_factory)
         self.__session = Session
 
-    def get(self, cls, id):
-        """
-        Retrieves one object based on the class name and its ID.
-        """
-        if cls not in classes:
-            return None
-        return self.__session.query(classes[cls]).filter_by(id=id).first()
-
     def close(self):
-        """Close the working SQLAlchemy session"""
+        """Close the working SQLAlchemy session."""
         self.__session.remove()
