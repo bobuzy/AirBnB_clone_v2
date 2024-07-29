0bbe4db958cac2cdf994055e41e3066df106de54
bobuzy <bobuzytech@gmail.com>
2024-07-29 15:39:18 +0000

    Render state lists

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Simple python flask script to list all states in the Database"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def list_states():
+    """Render all states in a template"""
+    states = storage.all(State)
+    state_objects = states.values()
+    sorted_states = sorted(state_objects, key=lambda state: state.name)
+    return render_template("7-states_list.html", states=sorted_states)
+
+@app.teardown_appcontext
+def stop_session(arg=None):
+    """Clean-up session"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
